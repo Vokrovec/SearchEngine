2bec044d0bbcb04735d578d05236b331b6138317
kratkvo1 <kratkvo1@fit.cvut.cz>
2026-07-29 18:17:58 +0200

    accualy run tokenizer

--- src/cpp/search.cpp
+++ src/cpp/search.cpp
@@ -1,5 +1,6 @@
 #include "search.h"
-//#include "Tokenizer.hpp"
+#include "Tokenizer.hpp"
+#include <fstream>
 #include <iostream>
 #include <filesystem>
 
@@ -7,12 +8,14 @@ void hello_world() {
     std::cout << "Hello world" << std::endl;
 }
 
-void tokenize_folder(const char* folderName) {
+void tokenize_folder(const char* folderName) { // must be a const char in argumnet, bcs golang accept only C interface
     std::filesystem::path folder = folderName;
-
+    Tokenizer T;
     for (const auto& entry: std::filesystem::directory_iterator(folder)) {
         if (std::filesystem::is_regular_file(entry)) {
             std::cout << "Tokenizing file: " << entry.path() << '\n';
+            std::fstream file(entry.path());
+            T.tokenize(file);
         }
     }
     std::cout.flush();
